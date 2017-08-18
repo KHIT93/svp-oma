342cffa0a6c838e0543c5daa3a9187fb2e438755
Daniel Mølgaard Nielsen <danielmlgaard@hotmail.com>
2017-08-18 13:52:56 +0200

    Made a change to a model, and found a solution to request method of the api

--- control/src/windturbine_data/views.py
+++ control/src/windturbine_data/views.py
@@ -5,15 +5,34 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models.windturbine_data import WindturbineData
 from .serializers import WindturbineDataSerializer
+import requests
+
+global data
 
 # windturbinedata/
 class WindturbineDataList(APIView):
 
     def get(self,request):
         #Få data fra database
-        data = WindturbineData.objects.all()
+        ##data = WindturbineData.objects.all()
         #Slet det hentede data fra DB
         #Send data afsted
         #Hvis der opstår en fejl, f.eks netværks problem skal den ikke slette noget
-        serializer = WindturbineDataSerializer(data, many=True)
+        ##serializer = WindturbineDataSerializer(data, many=True)
+
+        data = WindturbineData.objects.all()
+
+        #Her skal bruges O&M serveren's IP
+        r = requests.get('https://google.com')
+
+        if r.status_code == 200:
+            cache.set('data', data)
+
+        data.delete()
+
+        serializer = WindturbineDataSerializer(cache.get('data'), many=True)
+
         return Response(serializer.data)
+
+
+
