34435a17add32eeb8eeeed4b45339b8a51499899
Kenneth Hansen <kenneth@khansen-it.dk>
2017-08-21 09:04:15 +0200

    Updated production configuration to match the configuration of POC-server

--- om/src/om/settings/production.py
+++ om/src/om/settings/production.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'appcore',
+    'dashboard',
+    'turbinemanagement',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'om.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'svp-oma',
+        'USER': 'svp-oma',
+        'PASSWORD': 'P@ssw0rd',
+        'HOST': '10.135.17.153',
+        'PORT': '5432',
     }
 }
 
