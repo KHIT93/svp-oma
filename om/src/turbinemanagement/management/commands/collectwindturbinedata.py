e5af7918a7154b662e99dca6fa2fa8aae252a3c4
Kenneth Hansen <kenneth@khansen-it.dk>
2017-08-23 08:25:02 +0200

    Fix missing typecast

--- om/src/turbinemanagement/management/commands/collectwindturbinedata.py
+++ om/src/turbinemanagement/management/commands/collectwindturbinedata.py
@@ -7,12 +7,12 @@ class Command(BaseCommand):
         print("Starting data sync from windturbines")
         for windturbine in WindTurbine.objects.all():
             if windturbine.ip_address == "0.0.0.0":
-                print("Windturbine " + windturbine + " skipped due to missing IP-address")
+                print("Windturbine " + str(windturbine) + " skipped due to missing IP-address")
             else:
-                print("Running data sync for windturbine " + windturbine + " at " + windturbine.ip_address)
+                print("Running data sync for windturbine " + str(windturbine) + " at " + windturbine.ip_address)
                 try:
                     r = requests.get('http://' + windturbine.ip_address + '/windturbinedata/all/')
                     if r.status_code == 200:
                         print(r)
                 except:
-                    print("The windturbine " + windturbine + " at " + windturbine.ip_address + " could not be reached")
+                    print("The windturbine " + str(windturbine) + " at " + windturbine.ip_address + " could not be reached")
