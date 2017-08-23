5ccd95b17e043d8b8f064e62e3e9f52fbb276154
Daniel Mølgaard Nielsen <danielmlgaard@hotmail.com>
2017-08-23 12:36:28 +0200

    Changed windturbine model, and added logic for date  in windturbinedata view

--- control/src/windturbine_data/views.py
+++ control/src/windturbine_data/views.py
@@ -7,29 +7,40 @@ from .models.windturbine_data import WindturbineData
 from .serializers import WindturbineDataSerializer
 import requests
 import datetime
+from django.utils import timezone
 from app_settings.models.app_setting import AppSetting
 
 # windturbinedata/
 class WindturbineDataList(APIView):
 
     def get(self,request):
-        data = WindturbineData.objects.all()
+        #data = WindturbineData.objects.all()
 
-        current_time = datetime.datetime.now()
-        print("Inden")
+        current_time = timezone.now()
+
+        lastsyncdate = timezone.now()
+
         if not AppSetting.objects.filter(key='lastsyncdate'):
-            print("False")
-        else:
-            print('Inden indsæt')
-            appsett = AppSetting(key='lastsyncdate', value=current_time)
+            appsett = AppSetting.objects.create(key='lastsyncdate', value=current_time)
             appsett.save()
-            print("Indsat tid og dato")
-        print("Efter")
+        else:
+            lastsyncdate = AppSetting.objects.get(key='lastsyncdate').value
+
+        data = WindturbineData.objects.filter(timestamp__range=(lastsyncdate,current_time))
+
         try:
             r = requests.get('http://10.135.17.51')
             if r.status_code == 200:
                 cache.set('data', data)
-                data.delete()
+
+                last = WindturbineData.objects.earliest('timestamp')
+
+                clean = WindturbineData.objects.filter(timestamp__range=(last.timestamp, lastsyncdate))
+                clean.delete()
+
+                appsett = AppSetting.objects.filter(key='lastsyncdate').update(value=current_time)
+                appsett.save()
+
         except:
             pass
 
