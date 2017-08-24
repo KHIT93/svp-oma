d498f3930a2c92b6d79d27d35c86c3cf23005d31
Kenneth Hansen <kenneth@khansen-it.dk>
2017-08-24 13:38:22 +0200

    Fix missing field in error code model

--- om/src/errors/models/error_code.py
+++ om/src/errors/models/error_code.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class ErrorCode(models.Model):
+    code = models.IntegerField()
     message = models.CharField(max_length=255)
     severity = models.IntegerField()
