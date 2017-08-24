6703f8b4ab687f9e641bbaad347486c788e5ddac
Kenneth Hansen <kenneth@khansen-it.dk>
2017-08-24 13:14:00 +0200

    Add resolved field to windturbine_error

--- om/src/turbinemanagement/models/windturbine_error.py
+++ om/src/turbinemanagement/models/windturbine_error.py
@@ -7,6 +7,7 @@ class WindTurbineError(BaseModel):
     timestamp = models.DateTimeField(auto_now_add=True)
     error_messge = models.TextField()
     error_code = models.IntegerField()
+    resolved = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.windturbine)
