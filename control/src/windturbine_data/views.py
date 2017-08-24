a219dd95cbafa8462a90cd319339d96b3d6b6c36
Daniel Mølgaard Nielsen <danielmlgaard@hotmail.com>
2017-08-24 09:45:15 +0200

    Changed windturbine_settings and made changes in windturbine_data view

--- control/src/windturbine_data/views.py
+++ control/src/windturbine_data/views.py
@@ -1,26 +1,20 @@
-from django.shortcuts import get_object_or_404
 from django.core.cache import cache
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models.windturbine_data import WindturbineData
 from .serializers import WindturbineDataSerializer
-import requests
-import datetime
 from django.utils import timezone
 from app_settings.models.app_setting import AppSetting
 
 # windturbinedata/
 class WindturbineDataList(APIView):
-
     def get(self,request):
-        #data = WindturbineData.objects.all()
 
         current_time = timezone.now()
+        lastsyncdate = current_time
 
-        lastsyncdate = timezone.now()
-
-        if not AppSetting.objects.filter(key='lastsyncdate'):
+        if not helpers.first(AppSetting.objects.filter(key='lastsyncdate')):
             appsett = AppSetting.objects.create(key='lastsyncdate', value=current_time)
             appsett.save()
         else:
@@ -28,25 +22,28 @@ class WindturbineDataList(APIView):
 
         data = WindturbineData.objects.filter(timestamp__range=(lastsyncdate,current_time))
 
-        try:
-            r = requests.get('http://10.135.17.51')
-            if r.status_code == 200:
-                cache.set('data', data)
+        cache.set('data', data)
+        data.delete()
 
-                last = WindturbineData.objects.earliest('timestamp')
+        if WindturbineData.objects.all():
+            last = WindturbineData.objects.earliest('timestamp')
 
-                clean = WindturbineData.objects.filter(timestamp__range=(last.timestamp, lastsyncdate))
-                clean.delete()
+            clean = WindturbineData.objects.filter(timestamp__range=(last.timestamp, lastsyncdate))
+            clean.delete()
 
-                appsett = AppSetting.objects.filter(key='lastsyncdate').update(value=current_time)
-                appsett.save()
-
-        except:
-            pass
+            appsett = AppSetting.objects.filter(key='lastsyncdate').update(value=current_time)
+            appsett.save()
 
         serializer = WindturbineDataSerializer(cache.get('data'), many=True)
 
         return Response(serializer.data)
+
+class helpers():
+    def first(query):
+        try:
+            return query.all()[0]
+        except:
+            return None
 
 
 
