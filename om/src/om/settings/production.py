f26c71947aee756e927dc65ea7a391f3e96bf94e
Kenneth Hansen <kenneth@khansen-it.dk>
2017-08-25 09:24:11 +0200

    Add automatic logging

--- om/src/om/settings/production.py
+++ om/src/om/settings/production.py
@@ -142,3 +142,25 @@ REST_FRAMEWORK = {
     ],
     'PAGE_SIZE': 10
 }
+
+# Logging configuration
+# https://docs.djangoproject.com/en/1.11/topics/logging/#configuring-logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, "logs", "om.log"),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
