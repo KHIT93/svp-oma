b73dd097b32bc7c207e6e9906d637643a0ccc7a3
Daniel Mølgaard Nielsen <danielmlgaard@hotmail.com>
2017-08-25 19:15:23 +0200

    Fixed bug, in windturbinedata view

--- control/src/windturbine_data/serializers.py
+++ control/src/windturbine_data/serializers.py
@@ -5,5 +5,4 @@ class WindturbineDataSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = WindturbineData
-        # fields = ('windturbine_id','state','temp_gearbox','temp_generator','rpm_generator','wind_speed')
         fields = '__all__'
--- control/src/windturbine_settings/models/windturbine_setting.py
+++ control/src/windturbine_settings/models/windturbine_setting.py
@@ -3,7 +3,7 @@ from django.db import models
 class WindturbineSetting(models.Model):
     windturbine = models.IntegerField(default=0)
     state = models.IntegerField()
-    brake = models.BooleanField(default=0)
+    brake = models.BooleanField(default=False)
     wind_speed = models.DecimalField(max_digits=10, decimal_places=2)
     wing_angle = models.DecimalField(max_digits=10, decimal_places=2)
     max_rpm_generator = models.IntegerField()
