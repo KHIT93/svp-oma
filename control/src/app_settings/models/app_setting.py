f177fcd4d2b955cc7612f7b500f9b756de5e73f8
Daniel Mølgaard Nielsen <danielmlgaard@hotmail.com>
2017-08-29 13:15:02 +0200

    Changed to models, and ready for sphinx documentation

--- control/src/app_settings/models/app_setting.py
+++ control/src/app_settings/models/app_setting.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 class AppSetting(models.Model):
+    """
+    The AppSetting class is responsible for the configuration data
+
+    Each entry includes these fields:
+
+    :param key: A character representation for the title of the value
+
+    :param value: A character representation that holds the value for the key
+    """
     key = models.CharField(max_length=255, unique=True)
     value = models.TextField()
 
--- control/src/windturbine_data/models/windturbine_data.py
+++ control/src/windturbine_data/models/windturbine_data.py
@@ -1,6 +1,31 @@
 from django.db import models
 
 class WindturbineData(models.Model):
+    """
+    The WindTurbineData class defines the format in which we store the sensor information that has been recieved from the windturbines.
+
+    Each entry includes these fields:
+
+    :param id: Unique identifier for the specific sensor reading
+
+    :param windturbine: Relationship to the windturbine that collected the sensor data. In the SQL database, this reference is stored as windturbine_id
+
+    :param timestamp: Timestamp in ISO 8601 for when the data was registered at the windturbine
+
+    :param state: A numeric representation for the windturbine state at the time of this sensor reading
+
+    :param temp_gearbox: Temperature reading in degrees Celcius from the gearbox
+
+    :param temp_generator: Temperature reading in degrees Celcius from the generator
+
+    :param rpm_generator: Reading of the current RPM registered at the generator
+
+    :param wind_speed: Current reading of the wind speed at the windturbine
+
+    :param wing_angle: The angle of the turbine wings
+
+    :param brake: Defines if the brakes are active
+    """
     windturbine = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
     state = models.IntegerField()
