acf61f1ec9c45f662a9b99f62c138c1de46cd80a
Dennis Asmussen <dennisasmussendk@gmail.com>
2017-08-30 11:04:44 +0200

    Testing writing errors to db

--- ml/src/repository/windturbine_error_repository.py
+++ ml/src/repository/windturbine_error_repository.py
@@ -22,6 +22,7 @@ class WindturbineErrorRepo(object):
 		sqlstatement = ("INSERT INTO turbinemanagement_windturbineerror (timestamp, error_message, error_code, windturbine_id, resolved) VALUES (%s, %s, %s, %s, %s) RETURNING id")
 		sqldata = (windturbine_error.timestamp, windturbine_error.error_message, windturbine_error.error_code, windturbine_error.windturbine_id, False)
 		self.cursor.execute(sqlstatement, sqldata)
+		self.cursor.commit()
 		windturbine_error.id = self.cursor.fetchone()[0]
 		self.windturbine_errors[windturbine_error.id] = windturbine_error
 		return windturbine_error
