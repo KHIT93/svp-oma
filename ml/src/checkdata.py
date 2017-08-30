2dccbd8cf5175cebbec017d55ac3bc96b0c66acf
Dennis Asmussen <dennisasmussendk@gmail.com>
2017-08-30 10:48:32 +0200

    Testing writing errors to db

--- ml/src/checkdata.py
+++ ml/src/checkdata.py
@@ -33,9 +33,9 @@ prediction = model.predict(machinelearningarray)
 i = 0
 for x in prediction:
 	print(x)
-	if x not 0:
+	if x > 0:
 		error = np.array(errorCodeRepo.get(x))
 		windturbine_error = WindturbineError(dataarray[i,7], dataarray[i,1], error[0,1], x, False)
 		windturbineErrorRepo.save(windturbine_error)
-
+	
 	i += 1
