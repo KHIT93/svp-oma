7ac3df1fa1145596d1dd0315a872ff97b6c66bd4
Kenneth Hansen <kenneth@khansen-it.dk>
2017-08-30 11:12:44 +0200

    Fix typo in serializer

--- om/src/turbinemanagement/serializers/windturbine_error_serializer.py
+++ om/src/turbinemanagement/serializers/windturbine_error_serializer.py
@@ -4,7 +4,7 @@ from turbinemanagement.models.windturbine_error import WindTurbineError
 class WindturbineErrorSerializer(serializers.ModelSerializer):
     class Meta:
         model = WindTurbineError
-        fields = ('id', 'windturbine', 'timestamp', 'error_messge', 'error_code')
+        fields = ('id', 'windturbine', 'timestamp', 'error_message', 'error_code')
 
 
     @staticmethod
