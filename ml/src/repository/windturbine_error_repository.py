434b0846f12a4609d89c2f8ec1c00ac54d216cc1
Dennis Asmussen <dennisasmussendk@gmail.com>
2017-08-30 12:44:19 +0200

    creating a connector class

--- ml/src/repository/windturbine_error_repository.py
+++ ml/src/repository/windturbine_error_repository.py
@@ -15,7 +15,7 @@ class WindturbineErrorRepo(object):
 	def save(self, windturbine_error):
 		sqlstatement = ("INSERT INTO turbinemanagement_windturbineerror (timestamp, error_message, error_code, windturbine_id, resolved) VALUES (%s, %s, %s, %s, %s) RETURNING id")
 		sqldata = (windturbine_error.timestamp, windturbine_error.error_message, windturbine_error.error_code, windturbine_error.windturbine_id, False)
-		windturbine_error.id = self.connector.execute()[0]
+		windturbine_error.id = self.connector.execute(sqlstatement, sqldata)[0]
 		self.windturbine_errors[windturbine_error.id] = windturbine_error
 		return windturbine_error
 
