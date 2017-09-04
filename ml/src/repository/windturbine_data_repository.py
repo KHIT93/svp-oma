130579c88200986f793d19e50571fc8e3d8395d5
Dennis Asmussen <dennisasmussendk@gmail.com>
2017-09-04 09:53:18 +0200

    Testing average of windturbine data

--- ml/src/repository/windturbine_data_repository.py
+++ ml/src/repository/windturbine_data_repository.py
@@ -25,6 +25,6 @@ class WindturbineDataRepo(object):
 		last_datetime = self.windturbine_data[-1][1]
 		mean_datetime = first_datetime + (last_datetime - first_datetime) / 2
 		order = [0, 2, 3, 4, 5, 6, 7, 8, 9]
-		self.windturbine_data = np.average(self.windturbine_data[:][order], axis=0)
+		self.windturbine_data = np.average(np.array(self.windturbine_data)[:, order], axis=0)
 		np.insert(self.windturbine_data, 1, mean_datetime)
 		print(self.windturbine_data)
