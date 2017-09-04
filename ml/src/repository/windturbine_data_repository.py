949b5b4c205f9362128ead31471df7022e13f7f8
Dennis Asmussen <dennisasmussendk@gmail.com>
2017-09-04 09:42:47 +0200

    Testing average of windturbine data

--- ml/src/repository/windturbine_data_repository.py
+++ ml/src/repository/windturbine_data_repository.py
@@ -19,12 +19,12 @@ class WindturbineDataRepo(object):
 	def get_new(self, windturbine_id):
 		sqlstatement = ("SELECT * FROM turbinemanagement_windturbinedata WHERE id > %s AND windturbine_id = %s ORDER BY id")
 		sql_data = (self.last_record, windturbine_id)
-		self.windturbine_data = self.connector.execute(sqlstatement, sql_data)
+		self.windturbine_data = np.array(self.connector.execute(sqlstatement, sql_data))
 		self.last_record = self.windturbine_data[-1,0]
 		first_datetime = self.windturbine_data[0,1]
 		last_datetime = self.windturbine_data[-1,1]
 		mean_datetime = first_datetime + (last_datetime - first_datetime) / 2
 		order = [0, 2, 3, 4, 5, 6, 7, 8, 9]
-		self.windturbine_data = np.average(np.array(self.windturbine_data)[:,order], axis=0)
+		self.windturbine_data = np.average(self.windturbine_data[:,order], axis=0)
 		np.insert(self.windturbine_data, 1, mean_datetime)
 		print(self.windturbine_data)
