33c5791d97891466326102aea4e97c3349efcac3
Dennis Asmussen <dennisasmussendk@gmail.com>
2017-09-06 09:31:45 +0200

    added handling of duty below 0

--- embedded/src/Motor.py
+++ embedded/src/Motor.py
@@ -28,6 +28,11 @@ class Motor:
 
 	def changespeed(self, wind_speed, wing_angle): 
 		self.duty = Decimal(wind_speed) - Decimal(wing_angle)
+		if self.duty < 0:
+			self.duty = 0
+
+		if self.duty > 100:
+			self.duty = 100
 		self.pwm.ChangeDutyCycle(self.duty)
 
 	def brake(self):
