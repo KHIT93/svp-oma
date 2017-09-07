884d7bccc18247b303e90af8e178401891ef740d
Dennis Asmussen <dennisasmussendk@gmail.com>
2017-09-07 08:31:01 +0200

    Added auditlog

--- ml/src/models/audit_log.py
+++ ml/src/models/audit_log.py
@@ -2,26 +2,26 @@ import base_model
 
 class AuditLog():
 	"""
-    The AuditLog class defines the model used for logging actions and events happening in the application.
-    This can fx. be when a user performs an action or when the system synchronizes data with the
-    windturbines.
+	The AuditLog class defines the model used for logging actions and events happening in the application.
+	This can fx. be when a user performs an action or when the system synchronizes data with the
+	windturbines.
 
-    Each entry has the following values:
+	Each entry has the following values:
 
-    :param timestamp: Timestamp stored in ISO-8601
+	:param timestamp: Timestamp stored in ISO-8601
 
-    :param user: Reference to the application user that caused the entry to be created.
+	:param user: Reference to the application user that caused the entry to be created.
 
-    :param name: The name of the user or system component that caused the entry to be created.
+	:param name: The name of the user or system component that caused the entry to be created.
 
-    :param message: A message describing the performed action
+	:param message: A message describing the performed action
 
-    :param api_response: If an API call was performed this contains the API response that was recieved by the server
-    """
+	:param api_response: If an API call was performed this contains the API response that was recieved by the server
+	"""
 	def __init__(self, timestamp, user, name, message, api_response):
-        self.timestamp = timestamp
-	    self.user = user
-	    self.name = name
-	    self.message = message
-	    self.api_response = api_response
+		self.timestamp = timestamp
+		self.user = user
+		self.name = name
+		self.message = message
+		self.api_response = api_response
 		
